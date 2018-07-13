43ea8495a4128872de9743b6fe082528e4f61685
Magdalena <m.mandat@gmail.com>
2018-07-13 12:17:16 +0100

    Studio class seted up

--- src/main/java/models/Studio.java
+++ src/main/java/models/Studio.java
@@ -1,4 +1,30 @@
 package models;
 
 public class Studio {
+
+    private int id;
+    private String name;
+
+    public Studio(){}
+
+    public Studio(String name){
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
