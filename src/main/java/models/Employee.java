09d761238450b18b543f356096e24b6d52b27144
Magdalena <m.mandat@gmail.com>
2018-07-13 11:36:21 +0100

    Employee seted up

--- src/main/java/models/Employee.java
+++ src/main/java/models/Employee.java
@@ -1,4 +1,17 @@
 package models;
 
+import java.util.List;
+
 public abstract class Employee {
+
+
+
+
+    public Employee() {
+
+    }
+
+
+
 }
+
--- src/main/java/models/Film.java
+++ src/main/java/models/Film.java
@@ -1,4 +1,58 @@
 package models;
 
+import java.util.List;
+
 public class Film {
+
+    private int id;
+    private String title;
+    private Director director;
+    private Studio studio;
+    private List<Actor> actors;
+
+    public Film(){}
+
+    public Film(String title, Director director, Studio studio){
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Director getDirector() {
+        return director;
+    }
+
+    public void setDirector(Director director) {
+        this.director = director;
+    }
+
+    public List<Actor> getActors() {
+        return actors;
+    }
+
+    public void setActors(List<Actor> actors) {
+        this.actors = actors;
+    }
+
+    public Studio getStudio() {
+        return studio;
+    }
+
+    public void setStudio(Studio studio) {
+        this.studio = studio;
+    }
 }
